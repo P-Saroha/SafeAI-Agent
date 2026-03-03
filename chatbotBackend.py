c7ca38f2cf8e2bd21860ff5a3e173c6eeeb067be
Parveen Saroha <parveensaroha2002@gmail.com>
2026-03-03 22:07:02 +0530

    adding memeory to chatbot using sqlite

--- chatbotBackend.py
+++ chatbotBackend.py
@@ -2,9 +2,10 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv, find_dotenv
+import sqlite3
 
 load_dotenv(find_dotenv())
 
@@ -18,8 +19,11 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
+
+conn = sqlite3.connect(database='chatbot_db', check_same_thread=False)
+
 # Checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn = conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -27,3 +31,12 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+
+
+def unique_thread_pointer():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
--- chatbotFrontend.py
+++ chatbotFrontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from chatbotBackend import chatbot
+from chatbotBackend import chatbot, unique_thread_pointer
 from langchain_core.messages import HumanMessage, AIMessage
 import uuid
 
@@ -38,7 +38,7 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = unique_thread_pointer()
 
 add_thread(st.session_state['thread_id'])
 
